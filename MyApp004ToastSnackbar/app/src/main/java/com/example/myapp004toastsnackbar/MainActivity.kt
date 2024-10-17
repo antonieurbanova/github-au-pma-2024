8cad58e5bd1662c70759302246c57acf82e69282
Antonie Urbanová <urbanova.antonie@seznam.cz>
2024-10-17 11:34:09 +0200

    #4 toast akce

--- MyApp004ToastSnackbar/app/src/main/java/com/example/myapp004toastsnackbar/MainActivity.kt
+++ MyApp004ToastSnackbar/app/src/main/java/com/example/myapp004toastsnackbar/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.myapp004toastsnackbar
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -9,7 +10,7 @@ import com.example.myapp004toastsnackbar.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivitzMainBinding
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,6 +20,16 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        //nastaveni akce pro tlacitko Toast
+        binding.btnShowToast.setOnClickListener {
+            //nepouzivat velke T / to se pouyvia pro komponentu
+            val toast = Toast.makeText(this, "Nazdar - mam hlad", Toast.LENGTH_LONG)
+            toast.show()
+            
+            // akce pro tlacitko Zobrazit Snackbar
+
+            
+            
         }
     }
 }
