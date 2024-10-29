8111ce0263d9b55bb39c15f01601476a11a6890a
Antonie Urbanová <urbanova.antonie@seznam.cz>
2024-10-29 13:34:05 +0100

    #8 binding a obsluha

--- MyApp003Objednavka/app/src/main/java/com/example/myapp003objednavka/MainActivity.kt
+++ MyApp003Objednavka/app/src/main/java/com/example/myapp003objednavka/MainActivity.kt
@@ -1,20 +1,48 @@
 package com.example.myapp003objednavka
 
 import android.os.Bundle
+import android.widget.RadioButton
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.myapp003objednavka.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+       // enableEdgeToEdge()
+        //setContentView(R.layout.activity_main)
+        /* ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }*/
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        title = "Objednávka Cupcake"
+
+        binding.btnObjednavka.setOnClickListener {
+            val cupcakeRbId = binding.rgCupcakes.checkedRadioButtonId
+
+            val cupcake = findViewById<RadioButton>(cupcakeRbId)
+
+            val posypka = binding.cbPosypka.isChecked
+            val jahoda = binding.cbJahoda.isChecked
+            val kousky = binding.cbKousky.isChecked
+
+            val objednavkaText = "Souhrn objednávky" +
+                    "${cupcake.text}" +
+                    (if(posypka) "; barevná posypka" else "") +
+                    (if(jahoda) "; čerstvá jahoda" else "") +
+                    (if(kousky) "; kousky čokolády" else "")
+
+            binding.tvObjednavka.text = objednavkaText
         }
+
     }
 }
