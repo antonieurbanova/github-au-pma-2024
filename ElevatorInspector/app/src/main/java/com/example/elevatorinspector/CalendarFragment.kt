b13975e674526cc35bb44ff524efd27d72b15df2
Antonie Urbanová <urbanova.antonie@seznam.cz>
2025-01-13 12:44:11 +0100

    #11 pokus

--- ElevatorInspector/app/src/main/java/com/example/elevatorinspector/CalendarFragment.kt
+++ ElevatorInspector/app/src/main/java/com/example/elevatorinspector/CalendarFragment.kt
@@ -2,9 +2,12 @@ package com.example.elevatorinspector
 
 import android.graphics.Color
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AlertDialog
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import com.example.elevatorinspector.databinding.FragmentCalendarBinding
 import com.prolificinteractive.materialcalendarview.DayViewDecorator
@@ -14,8 +17,10 @@ import com.prolificinteractive.materialcalendarview.CalendarDay
 import com.prolificinteractive.materialcalendarview.DayViewFacade
 import com.prolificinteractive.materialcalendarview.MaterialCalendarView
 import com.prolificinteractive.materialcalendarview.spans.DotSpan
+import java.text.SimpleDateFormat
 import java.util.Calendar
 import java.util.Date
+import java.util.Locale
 
 class CalendarFragment : Fragment() {
     private var _binding: FragmentCalendarBinding? = null
@@ -33,7 +38,10 @@ class CalendarFragment : Fragment() {
         // Načti data z Firebase a aplikuj dekorátory
         fetchFirebaseData(calendarView)
 
-        // Zde můžete přidat logiku pro zvýraznění dnů, přidání dekorátorů atd.
+        // Nastavit posluchač pro kliknutí na den
+        calendarView.setOnDateChangedListener { _, date, _ ->
+            onDateSelected(date)
+        }
 
         return root
     }
@@ -65,7 +73,7 @@ class CalendarFragment : Fragment() {
             plannedOP?.let { plannedDates.add(dateToCalendarDay(it)) }
             plannedOZ?.let { plannedDates.add(dateToCalendarDay(it)) }
         }
-
+        val plannedColor = ContextCompat.getColor(requireContext(), R.color.blue)
         calendarView.addDecorator(EventDecorator(color, plannedDates))
     }
 
@@ -74,14 +82,34 @@ class CalendarFragment : Fragment() {
         calendarView: MaterialCalendarView,
         color: Int
     ) {
-        val inspectionDates = mutableSetOf<CalendarDay>()
+
+        val vcasDates = mutableSetOf<CalendarDay>()
+        val pozdeDates = mutableSetOf<CalendarDay>()
+
         for (doc in snapshot.documents) {
             val date = doc.getTimestamp("datum")?.toDate()
-            date?.let { inspectionDates.add(dateToCalendarDay(it)) }
+            val vcas = doc.getBoolean("vcas") ?: true
+
+            date?.let {
+                val calendarDay = dateToCalendarDay(it)
+                if (vcas) {
+                    vcasDates.add(calendarDay)
+                } else {
+                    pozdeDates.add(calendarDay)
+                }
+            }
         }
 
-        calendarView.addDecorator(EventDecorator(color, inspectionDates))
+        val vcasColor = ContextCompat.getColor(requireContext(), R.color.green)
+        val pozdeColor = ContextCompat.getColor(requireContext(), R.color.red)
+
+        calendarView.addDecorator(EventDecorator(vcasColor, vcasDates))
+        calendarView.addDecorator(EventDecorator(pozdeColor, pozdeDates))
+
     }
+
+
+    // Pomocná funkce pro převod Date na CalendarDay
 
     private fun dateToCalendarDay(date: Date): CalendarDay {
         val calendar = Calendar.getInstance()
@@ -93,21 +121,153 @@ class CalendarFragment : Fragment() {
         )
     }
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
+    private fun dateToDate(calendarDay: CalendarDay): Date {
+        val calendar = Calendar.getInstance()
+        calendar.set(calendarDay.year, calendarDay.month - 1, calendarDay.day)
+        return calendar.time
+    }
+
+    private fun onDateSelected(date: CalendarDay) {
+        val selectedDate = dateToDate(date)
+        //Log.d("CalendarFragment", "Vybraný datum: $selectedDate")  // Logování vybraného data
+
+        val calendar = Calendar.getInstance()
+        calendar.time = selectedDate
+        calendar.set(Calendar.HOUR_OF_DAY, 0)
+        calendar.set(Calendar.MINUTE, 0)
+        calendar.set(Calendar.SECOND, 0)
+        calendar.set(Calendar.MILLISECOND, 0)
+
+        val startOfDay = calendar.time
+        calendar.add(Calendar.DAY_OF_YEAR, 1)
+        val endOfDay = calendar.time
+
+        //Log.d("CalendarFragment", "Načítání z Firebase pro datum: $startOfDay až $endOfDay")  // Logování doby načítání
+
+        db.collection("vytahy")
+            .whereGreaterThanOrEqualTo("plannedOP", startOfDay)
+            .whereLessThanOrEqualTo("plannedOP", endOfDay)
+            .get()
+            .addOnSuccessListener { opSnapshot ->
+                val records = mutableListOf<String>()
+
+                for (doc in opSnapshot.documents) {
+                    records.add(
+                        "Poslední den pro provedení včasné OP pro ${
+                            doc.getString("druh")?.lowercase()
+                        } výtah ${doc.getString("nazev")} na adrese ${doc.getString("ulice")} ${
+                            doc.getString(
+                                "cisloPopisne"
+                            )
+                        }, ${doc.getString("mesto")}!"
+                    )
+                }
+
+                db.collection("vytahy")
+                    .whereGreaterThanOrEqualTo("plannedOZ", startOfDay)
+                    .whereLessThanOrEqualTo("plannedOZ", endOfDay)
+                    .get()
+                    .addOnSuccessListener { ozSnapshot ->
+
+                        for (doc in ozSnapshot.documents) {
+                            records.add(
+                                "Měla by být provedena OZ pro ${
+                                    doc.getString("druh")?.lowercase()
+                                } výtah ${doc.getString("nazev")} na adrese ${doc.getString("ulice")} ${
+                                    doc.getString(
+                                        "cisloPopisne"
+                                    )
+                                }, ${doc.getString("mesto")}."
+                            )
+                        }
+
+
+                        db.collection("prohlidky")
+                            .whereGreaterThanOrEqualTo("datum", startOfDay)
+                            .whereLessThanOrEqualTo("datum", endOfDay)
+                            .get()
+                            .addOnSuccessListener { inspectionsSnapshot ->
+                                //Log.d("CalendarFragment", "Dostala jsem se do dovnitř: $startOfDay až $endOfDay")  // Logování doby načítání
+
+                                // Loguj počet dokumentů, které byly vráceny dotazem
+                                //Log.d("CalendarFragment", "Počet prohlídek: ${inspectionsSnapshot.size()}")
+
+
+                                for (doc in inspectionsSnapshot.documents) {
+                                    val vcas = doc.getBoolean("vcas") ?: false
+                                    val status = if (vcas) "včas" else "pozdě"
+
+                                        records.add(
+                                            "${doc.getString("jmenoInspektora")} provedl/a ${
+                                                doc.getString(
+                                                    "prohlidkaTyp"
+                                                )
+                                            } pro ${
+                                                doc.getString("druh")?.lowercase()
+                                            } výtah ${doc.getString("nazev")} na adrese ${
+                                                doc.getString(
+                                                    "ulice"
+                                                )
+                                            } ${doc.getString("cisloPopisne")}, ${
+                                                doc.getString(
+                                                    "mesto"
+                                                )
+                                            }. ${
+                                                doc.getString(
+                                                    "prohlidkaTyp"
+                                                )
+                                            } byla provedena $status."
+                                        )
+                                    }
+
+                                if (records.isEmpty()) {
+                                    records.add("Žádné záznamy pro tento den.")
+                                }
+
+                                // Log.d("CalendarFragment", "Záznamy pro tento den: $records")  // Logování záznamů
+
+                                showRecordsDialog(records, selectedDate)
+                            }
+                    }
+            }
+        }
+
+    private fun showRecordsDialog(records: List<String>, date: Date) {
+        // Formátování data na "den/měsíc/rok"
+        val dateFormat = SimpleDateFormat("dd/MM/yyyy", Locale.getDefault())
+        val formattedDate = dateFormat.format(date)
+
+        val message = if (records.isNotEmpty()) {
+            records.joinToString("\n")
+        } else {
+            "Pro tento den nebyly nalezeny žádné záznamy."
+        }
+
+        AlertDialog.Builder(requireContext())
+            .setTitle("Záznamy pro ${formattedDate}")
+            .setMessage(message)
+            .setPositiveButton("OK", null)
+            .show()
     }
 
 
-    // Dekorátor pro zvýraznění dnů
-    class EventDecorator(private val color: Int, private val dates: Set<CalendarDay>) :
-        DayViewDecorator {
+    // Třída dekorátoru
+    class EventDecorator(
+        private val color: Int,
+        private val dates: Set<CalendarDay>
+    ) : DayViewDecorator {
         override fun shouldDecorate(day: CalendarDay): Boolean {
             return dates.contains(day)
         }
 
         override fun decorate(view: DayViewFacade) {
-            view.addSpan(DotSpan(8f, color)) // Přidání barevného tečkovaného zvýraznění
+            view.addSpan(DotSpan(8f, color)) // Přidání tečky s příslušnou barvou
         }
     }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
+
